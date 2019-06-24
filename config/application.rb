331fe85aa3df8df21fa665ac40b12557d8a6ca38
Ayana Bando <ayanaflax@yahoo.com>
2019-06-24 14:04:33 -0400

    add eager load to custom classes

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,21 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+# Eager load all value objects, as they may be instantiated from
+# YAML before the symbol is referenced
+config.before_initialize do |app|
+    app.config.paths.add 'app/values', :eager_load => true
+end
+
+# Reload cached/serialized classes before every request (in development
+# mode) or on startup (in production mode)
+config.to_prepare do
+    Dir[ File.expand_path(Rails.root.join("app/values/*.rb")) ].each do |file|
+        require_dependency file
+    end
+    require_dependency 'article_cache'
+end
+
 module RailsCacheBugNew
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
