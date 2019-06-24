73c33f11ae8dcd89c129dc06baf8968f68656f8b
Ayana Bando <ayanaflax@yahoo.com>
2019-06-24 08:54:48 -0400

    add avatar parameters to contoller

--- db/schema.rb
+++ db/schema.rb
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2019_06_19_233402) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-5.times { User.create }
+# user = User.create(name: 'James Bond')
+  5.times { User.create }
