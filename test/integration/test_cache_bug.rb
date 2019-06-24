d1612731827d6fd9e7c9a013a6a5de5dfacb404a
Ayana Bando <ayanaflax@yahoo.com>
2019-06-24 11:49:16 -0400

    rebuild app and pass all tests

---
+++ test/integration/test_cache_bug.rb
@@ -0,0 +1,30 @@
+require 'test_helper'
+
+# This test, calling user.id, passes
+class CacheBugTest < ActionDispatch::IntegrationTest
+  test "cached entries should not change part a" do
+    f = Rails.cache.fetch("key1") do
+      users = User.first(5)
+      users[0].id
+      [users, 0]
+    end
+
+    r = Rails.cache.read("key1")
+
+    assert_equal(f[1], r[1])
+  end
+end
+
+# This test, calling user.avatar, fails
+class CacheBugTest < ActionDispatch::IntegrationTest
+  test "cached entries should not change part b" do
+    f = Rails.cache.fetch("key2") do
+      users = User.first(5)
+      [users, 0]
+    end
+
+    r = Rails.cache.read("key2")
+
+    assert_equal(f[1], r[1])
+  end
+end
